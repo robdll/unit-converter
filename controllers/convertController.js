f7e4fa42adc496f92c28fce0a3722e56ffee733a
robdll <dilillo.roberto@gmail.com>
2022-08-07 14:08:31 -0400

    avoid leading 0

--- controllers/convertController.js
+++ controllers/convertController.js
@@ -9,7 +9,8 @@ function convertController() {
       if (result.indexOf("/") >= 0) {
         let num1 = result.split("/")[0];
         let num2 = result.split("/")[1];
-        result = !num1 || !num2 ? null : (num1 / num2).toFixed(5);
+        result =
+          !num1 || !num2 ? null : Number.parseFloat((num1 / num2).toFixed(5));
       } else {
         result =
           result.indexOf(".") >= 0
