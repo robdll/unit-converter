61f9a42baf701400107cdeadc2987c3283d397ff
robdll <dilillo.roberto@gmail.com>
2022-08-07 15:02:14 -0400

    adds redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/_api/get-tests",
+        destination: "/api/fake-tests",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
